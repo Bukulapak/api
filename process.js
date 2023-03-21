276292ca0a4e0ba41913bd5da43c2041f58fadd3
Indra Riksa Herlambang <indrariksa@gmail.com>
2023-03-21 12:34:09 +0700

    Create process.js

---
+++ process.js
@@ -0,0 +1,51 @@
+export function postWithToken(target_url,tokenkey,tokenvalue,datajson,responseFunction){
+    var myHeaders = new Headers();
+    myHeaders.append(tokenkey, tokenvalue);
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify(datajson);
+
+    var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+    };
+
+    fetch(target_url, requestOptions)
+    .then(response => response.text())
+    .then(result => responseFunction(JSON.parse(result)))
+    .catch(error => console.log('error', error));
+}
+
+export function postWithBearer(target_url,token,datajson,responseFunction){
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer "+token);
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify(datajson);
+
+    var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+    };
+
+    fetch(target_url, requestOptions)
+    .then(response => response.text())
+    .then(result => responseFunction(JSON.parse(result)))
+    .catch(error => console.log('error', error));
+}
+
+export function get(target_url,responseFunction){
+    var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+    };
+
+    fetch(target_url, requestOptions)
+    .then(response => response.text())
+    .then(result => responseFunction(JSON.parse(result)))
+    .catch(error => console.log('error', error));
+}
