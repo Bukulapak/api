7500350ff39bd838d90bdadeb15677932ec6a59a
Indra Riksa Herlambang <indrariksa@gmail.com>
2023-05-18 19:12:23 +0700

    Update process.js

--- process.js
+++ process.js
@@ -95,7 +95,7 @@ export function get(target_url,responseFunction){
     };
 
     fetch(target_url, requestOptions)
-    .then(response => response.json())
+    .then(response => response.text())
     .then(result => responseFunction(JSON.parse(result)))
     .catch(error => console.log('error', error));
 }
